a43c642a23f044c0a55ed2553a8312f143a5a906
elkuku <der.el.kuku@gmail.com>
2025-12-26 11:08:32 -0500

    Add github scripts

---
+++ .github/scripts/build.js
@@ -0,0 +1,56 @@
+import fs from 'fs'
+
+if (!fs.existsSync('dist')) fs.mkdirSync('dist')
+
+const themes = fs.readdirSync('themes', {withFileTypes: true})
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+
+const themesList = {}
+let cssDefinitions = ''
+
+themesList.default = {
+    name: 'Default IITC',
+    css: '',
+    preview: 'https://github.com/Falenone/Blurtheme/blob/main/images/prev-default.png?raw=true',
+}
+
+themes.forEach(theme => {
+    const meta = JSON.parse(fs.readFileSync(`themes/${theme}/meta.json`))
+
+    let cssString = fs.readFileSync(`themes/${theme}/main.css`, 'utf8') + '\n'
+    const additionalCssFiles = fs.readdirSync(`themes/${theme}`)
+        .filter(file => file.endsWith('.css'))
+        .filter(file => !file.startsWith('main.css'))
+    additionalCssFiles.forEach(file => {
+        cssString += fs.readFileSync(`themes/${theme}/${file}`, 'utf8') + '\n'
+    })
+
+    // cssString = cssString.replace(/ {4}|[\r\n\t]/g, '')
+
+    // cssDefinitions += `const ${theme}CSS = '${cssString}'\n`
+
+    themesList[theme] = {
+        name: meta.name,
+        css: cssString.replace(/ {4}|[\r\n\t]/g, ''),
+        preview: meta.preview,
+    }
+})
+
+const fileName = 'IITC-plugin-Blurtheme.template.js'
+
+let template = fs.readFileSync(fileName, 'utf8') + '\n'
+
+const replacement =
+    cssDefinitions
+    + '\n\n'
+    + 'const themesList = ' + JSON.stringify(themesList, null, 2)
+
+fs.writeFileSync(
+    'dist/' + fileName.replace('.template.', '.user.'),
+    template.replace('// {{THEMES_LIST}}', replacement),
+)
+
+fs.writeFileSync(
+    'dist/' + fileName.replace('.template.', '.meta.'),
+    '')
